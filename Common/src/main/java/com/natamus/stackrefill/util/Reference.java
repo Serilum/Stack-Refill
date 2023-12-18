143f8b0eff219e93a891b1d2cc724243448b1331
Rick South <rick@natamus.com>
2023-12-18 10:00:30 +0100

    (v4.2) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/stackrefill/util/Reference.java
+++ Common/src/main/java/com/natamus/stackrefill/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.stackrefill.util;
 public class Reference {
 	public static final String MOD_ID = "stackrefill";
 	public static final String NAME = "Stack Refill";
-	public static final String VERSION = "4.1";
+	public static final String VERSION = "4.2";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
