08274a712d6617478e3f0adf73ab4a157dc5064d
Rick South <rick@natamus.com>
2025-02-12 23:12:15 +0100

    (v4.6) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/stackrefill/forge/events/ForgeRefillEvent.java
+++ Forge/src/main/java/com/natamus/stackrefill/forge/events/ForgeRefillEvent.java
@@ -12,16 +12,14 @@ import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.player.PlayerDestroyItemEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 import java.util.HashMap;
 
-@EventBusSubscriber
 public class ForgeRefillEvent {
 	private static final HashMap<String, InteractionHand> lasthandused = new HashMap<String, InteractionHand>();
 
 	@SubscribeEvent
-	public void onWorldTick(TickEvent.ServerTickEvent e) {
+	public static void onWorldTick(TickEvent.ServerTickEvent e) {
 		if (!e.phase.equals(Phase.START)) {
 			return;
 		}
@@ -30,7 +28,7 @@ public class ForgeRefillEvent {
 	}
 
 	@SubscribeEvent
-	public void onItemUse(LivingEntityUseItemEvent.Start e) {
+	public static void onItemUse(LivingEntityUseItemEvent.Start e) {
 		Entity livingEntity = e.getEntity();
 		if (!(livingEntity instanceof Player)) {
 			return;
@@ -49,7 +47,7 @@ public class ForgeRefillEvent {
 	}
 
 	@SubscribeEvent
-	public void onItemUse(LivingEntityUseItemEvent.Finish e) {
+	public static void onItemUse(LivingEntityUseItemEvent.Finish e) {
 		Entity livingEntity = e.getEntity();
 		if (!(livingEntity instanceof Player)) {
 			return;
@@ -65,22 +63,22 @@ public class ForgeRefillEvent {
 	}
 	
 	@SubscribeEvent
-	public void onItemBreak(PlayerDestroyItemEvent e) {
+	public static void onItemBreak(PlayerDestroyItemEvent e) {
 		RefillEvent.onItemBreak(e.getEntity(), e.getOriginal(), e.getHand());
 	}
 	
 	@SubscribeEvent
-	public void onItemToss(ItemTossEvent e) {
+	public static void onItemToss(ItemTossEvent e) {
 		RefillEvent.onItemToss(e.getPlayer(), e.getEntity().getItem());
 	}
 	
 	@SubscribeEvent
-	public void onItemRightClick(PlayerInteractEvent.RightClickItem e) {
+	public static void onItemRightClick(PlayerInteractEvent.RightClickItem e) {
 		RefillEvent.onItemRightClick(e.getEntity(), e.getLevel(), e.getHand());
 	}
 	
 	@SubscribeEvent
-	public void onBlockRightClick(PlayerInteractEvent.RightClickBlock e) {
+	public static void onBlockRightClick(PlayerInteractEvent.RightClickBlock e) {
 		RefillEvent.onBlockRightClick(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec());
 	}
 }
