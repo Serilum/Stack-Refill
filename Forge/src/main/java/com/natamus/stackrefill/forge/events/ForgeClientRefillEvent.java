e776a173a48a9467e3d278eba5ce8288e146fc00
Rick South <rick@natamus.com>
2025-07-01 09:53:43 +0200

    (v4.9) Fixed Forge client events not being static.

--- Forge/src/main/java/com/natamus/stackrefill/forge/events/ForgeClientRefillEvent.java
+++ Forge/src/main/java/com/natamus/stackrefill/forge/events/ForgeClientRefillEvent.java
@@ -1,15 +1,12 @@
 package com.natamus.stackrefill.forge.events;
 
 import com.natamus.stackrefill.events.RefillEvent;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(Dist.CLIENT)
 public class ForgeClientRefillEvent {
-    @SubscribeEvent()
-    public void onWorldTick(TickEvent.ClientTickEvent e) {
+    @SubscribeEvent
+    public static void onWorldTick(TickEvent.ClientTickEvent e) {
         if (!e.phase.equals(TickEvent.Phase.START)) {
             return;
         }
