2de879988bcf2e899c42ee8539270a2cf1753e74
Rick South <rick@natamus.com>
2025-10-04 19:27:55 +0200

    (v5.0) (1.21.9 Forge) Fixed incorrect remnant files included in the mod jar.

--- Common/src/main/java/com/natamus/stackrefill/util/Reference.java
+++ Common/src/main/java/com/natamus/stackrefill/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.stackrefill.util;
 public class Reference {
 	public static final String MOD_ID = "stackrefill";
 	public static final String NAME = "Stack Refill";
-	public static final String VERSION = "4.9";
+	public static final String VERSION = "5.0";
 	public static final String ACCEPTED_VERSIONS = "[1.21.9]";
 }
